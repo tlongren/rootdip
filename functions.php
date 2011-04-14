60a27536326e67060dfba659b94f05bb42920f13
Tyler Longren <tlongren@gmail.com>
2011-04-14 17:30:52 -0600

    Fixed issue #1

--- functions.php
+++ functions.php
@@ -18,4 +18,26 @@ if(function_exists('register_sidebar')){
 		'after_title' => "</h3>\n"
 	));
 }
+add_filter('comment_form_default_fields', 'html5press_comments');
+function html5press_comments() {
+	$req = get_option('require_name_email');
+	$fields =  array(
+'author' => '<p>' . '<label for="author">' . __( 'Name' ) . '</label> ' . ( $req ? '<span>*</span>' : '' ) .
+'<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' placeholder = "What should we call you?"' . ( $req ? ' required' : '' ) . '/></p>',
+
+'email'  => '<p><label for="email">' . __( 'Email' ) . '</label> ' . ( $req ? '<span>*</span>' : '' ) .
+'<input id="email" name="email" type="email" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' placeholder="How can we reach you?"' . ( $req ? ' required' : '' ) . ' /></p>',
+
+'url'    => '<p><label for="url">' . __( 'Website' ) . '</label>' .
+'<input id="url" name="url" type="url" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" placeholder="Have you got a website?" /></p>'
+);
+	return $fields;
+}
+
+add_filter('comment_form_field_comment', 'html5press_commentfield');
+function html5press_commentfield() {
+	$commentArea = '<p><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true" required placeholder="What\'s on your mind?"    ></textarea></p>';
+	return $commentArea;
+}
+
 ?>
