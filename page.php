ea78f786c6e818f743c819155f716a8c7b386a38
Tyler Longren <tlongren@gmail.com>
2011-04-16 01:34:55 -0500

    Linked titles to the permalink.

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <article class="post">
         
-            <h2><a href="#"><?php the_title(); ?></a></h2>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
             
             <?php the_content(__('Read more')); ?>
             
