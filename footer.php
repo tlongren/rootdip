abf7c932c92479a6954f9b6c458516549aca704a
Tyler Longren <tlongren@gmail.com>
2011-04-15 23:09:42 -0500

    Dynamic year for copyright in footer.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     
     <footer id="footer" class="grid_12">
         <!-- You're free to remove the credit link in the footer, but please, please leave it there. -->
-        <p>Copyright &copy; 2011 <a href="<?php bloginfo('url'); ?>" title="This Website"><?php bloginfo('name'); ?></a> - Design by <a href="http://jayj.dk" title="Design by Jayj.dk">Jayj.dk</a> & WordPress'd by <a href="http://www.longren.org/wordpress/html5press/">Tyler Longren</a></p>
+        <p>Copyright &copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>" title="This Website"><?php bloginfo('name'); ?></a> - Design by <a href="http://jayj.dk" title="Design by Jayj.dk">Jayj.dk</a> & WordPress'd by <a href="http://www.longren.org/wordpress/html5press/">Tyler Longren</a></p>
     </footer> <!-- end footer -->
     
     <div class="clear"></div>
