c32751b5e20dd08e033e9a9106ccd4dc560f346a
Tyler Longren <tlongren@gmail.com>
2011-04-16 09:59:18 -0500

    Added next/previous post  links.

--- single.php
+++ single.php
@@ -5,6 +5,8 @@
         <article class="post">
         
             <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<div class="alignleft"><?php previous_post_link(); ?></div><div class="alignright"><?php next_post_link(); ?></div>
+            <div class="clear"></div>
             <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' ); ?>
 			<?php $large_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' ); ?>
             <?php if ( has_post_thumbnail() ) { ?><a href="<?php echo "$large_image[0]"; ?>" rel="thumbnail"><img src="<?php echo "$image[0]"; ?>" alt="" class="thumbnail alignleft" /></a><?php } ?>
@@ -14,7 +16,7 @@
             <div class="clear"></div>
 			
 			<footer class="postmeta">
-                <span class="btn alignleft">
+				<span class="btn alignleft">
                 	In <?php the_category(', '); ?> by <?php the_author(); ?> on <a href="<?php bloginfo('url'); ?>/<?php echo get_the_time('Y/m'); ?>"><time datetime="<?php echo get_the_time('Y-m-d'); ?>" pubdate><?php echo get_the_time( get_option( 'date_format' ) ); ?></time></a>
 				</span>
 				<?php /* Edit Link */ edit_post_link( __('Edit entry','html5press'), '<span class="btn alignright">', '</span>' ); ?>
