5816ed2563ac809a52cd979d023d3402c22fcacb
Tyler Longren <tlongren@gmail.com>
2011-04-16 01:52:08 -0500

    Added initial attachment template.

---
+++ attachment.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+
+<div id="main" class="grid_8 alpha">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article class="post">
+        
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        
+<?php if ( wp_attachment_is_image( $post->id ) ) : $att_image = wp_get_attachment_image_src( $post->id, "medium"); ?>
+			<figure>
+			<a href="<?php echo wp_get_attachment_url($post->id); ?>" title="<?php the_title(); ?>" rel="attachment"><img src="<?php echo $att_image[0];?>" width="<?php echo $att_image[1];?>" height="<?php echo $att_image[2];?>"  class="attachment-medium" alt="<?php $post->post_excerpt; ?>" /></a>
+			<figcaption><?php if ( !empty($post->post_excerpt) ) the_excerpt() ?></figcaption>
+			</figure>
+<?php else : ?>
+			<a href="<?php echo wp_get_attachment_url($post->ID) ?>" title="<?php echo wp_specialchars( get_the_title($post->ID), 1 ) ?>" rel="attachment"><?php echo basename($post->guid) ?></a>
+			<div class="entry-caption"><?php if ( !empty($post->post_excerpt) ) the_excerpt() ?></div>
+<?php endif; ?>
+
+			<div class="clear"></div>
+			
+			<footer class="postmeta">
+                <span class="btn alignleft">
+                	Created <time datetime="<?php echo get_the_time('Y-m-d'); ?>" pubdate><?php echo get_the_time( get_option( 'date_format' ) ); ?></time>
+				</span>
+				<?php /* Edit Link */ edit_post_link( __('Edit attachment','html5press'), '<span class="btn alignright">', '</span>' ); ?>
+            </footer> <!-- end post meta -->
+			<article class="comments">
+				<?php comments_template(); ?>
+			</article>
+        </article> <!-- end post 1 -->
+		<?php endwhile; endif; ?>
+    
+    </div> <!-- end main -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
