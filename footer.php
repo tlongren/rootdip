30fa067209408c8d015b4f4f5a3c9d6eb74847e0
Tyler Longren <tlongren@gmail.com>
2011-04-21 14:58:07 -0500

    Switched to UItoTop jQuery plugin for the "back to top" button. It works much better than the previous "back to top" code that was introduced in commit 4821bc286df6b56c8c38.

--- footer.php
+++ footer.php
@@ -20,7 +20,14 @@
     <div class="clear"></div>
 
 </div> <!-- end wrapper -->
-<div id="message"><a href="#top">Scroll to top</a></div> 
+	<script src="<?php echo get_template_directory_uri(); ?>/js/easing.js" type="text/javascript"></script> 
+	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.ui.totop.js" type="text/javascript"></script>
+	<script type="text/javascript">
+	var $j = jQuery.noConflict();
+	$j(document).ready(function() {		
+		$j().UItoTop({ easingType: 'easeOutQuart',text: 'Back To Top',min: '300'});
+	});
+	</script> 
 	<?php wp_footer(); ?>
 </body>
 </html>
