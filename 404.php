79bd4c79562c4962e8b747e114a84065ab5e76a2
Tyler Longren <tlongren@gmail.com>
2011-05-04 14:41:36 -0500

    Add hentry class to 404 page so H2 is styled properly.

--- 404.php
+++ 404.php
@@ -1,15 +1,12 @@
 <?php get_header(); ?>
 
 <div id="main" class="grid_8 alpha">
-        <article <?php post_class(); ?>>
-        
-            <h2><?php _e( 'Error 404','html5press'); ?></h2>
-            <p><?php _e( 'The page you requested could not be found. Try searching or going back to the ','html5press'); ?><a href="<?php home_url(); ?>"><?php _e( 'home page','html5press'); ?></a>.</p>
-            <div class="clear"></div>
-        
-        </article> <!-- end post 1 -->
-    
-    </div> <!-- end main -->
+	<article class="hentry">
+        <h2><?php _e( 'Error 404','html5press'); ?></h2>
+		<p><?php _e( 'The page you requested could not be found. Try searching or going back to the ','html5press'); ?><a href="<?php home_url(); ?>"><?php _e( 'home page','html5press'); ?></a>.</p>
+		<div class="clear"></div>
+	</article> <!-- end post 1 --> 
+</div> <!-- end main -->
 
 <?php get_sidebar(); ?>
 
