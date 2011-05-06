a886a8a97d81adf255085b8030d322834d790167
Tyler Longren <tlongren@gmail.com>
2011-05-06 15:18:57 -0500

    Temporarily remove validation for number of featured posts option, so that feature works. Something with that validation is failing, causing the value to not be saved in the database.

--- theme-options.php
+++ theme-options.php
@@ -175,10 +175,10 @@ function html5press_validate_options( $input ) {
 		$input['featured_cat'] = $prev;
 		
 	// We select the previous value of the field, to restore it in case an invalid entry has been given
-	$prev = $settings['num_featured'];
+	// $prev = $settings['num_featured'];
 	// We verify if the given value exists in the categories array
-	if ( !array_key_exists( $input['num_featured'], $html5press_num_featured_options ) )
-		$input['num_featured'] = $prev;
+	// if ( !array_key_exists( $input['num_featured'], $html5press_num_featured_options ) )
+		// $input['num_featured'] = $prev;
 	
 	// We select the previous value of the field, to restore it in case an invalid entry has been given
 	$prev = $settings['featured_image_size'];
@@ -195,3 +195,4 @@ function html5press_validate_options( $input ) {
 }
 
 endif;  // EndIf is_admin()
+?>
