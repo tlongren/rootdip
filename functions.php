dda2c6c101d2dde2fdf66fa4b39417fb0a25f5aa
Tyler Longren <tlongren@gmail.com>
2011-05-09 09:53:47 -0500

    Not getting much better than this. Might as well call this 1.4 stable.

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ function html5press_layout_view() {
 if ( ! isset( $content_width ) ) $content_width = 580;
 
 // Set html5press version
-define( 'html5press_version', '1.4-rc2' );
+define( 'html5press_version', '1.4' );
 function html5press_getinfo( $show = '' ) {
         $output = '';
 
