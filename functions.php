50059ce3ed337eb191479131e07447b9b13aea06
Tyler Longren <tlongren@gmail.com>
2011-05-26 14:34:47 -0500

    HTML5Press 1.5.

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ function html5press_layout_view() {
 if ( ! isset( $content_width ) ) $content_width = 580;
 
 // Set html5press version
-define( 'html5press_version', '1.5-rc1' );
+define( 'html5press_version', '1.5' );
 function html5press_getinfo( $show = '' ) {
         $output = '';
 
