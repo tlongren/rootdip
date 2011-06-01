c2eadb8b676191f855f42681072e9908ffbe4474
Tyler Longren <tlongren@gmail.com>
2011-06-01 13:29:29 -0500

    Don't call the_excerpt. It causes the Jetpack plugin share widgets to be included with the image caption, which obviously breaks page layout.

--- attachment.php
+++ attachment.php
@@ -10,11 +10,11 @@
 <?php if ( wp_attachment_is_image( $post->id ) ) : $att_image = wp_get_attachment_image_src( $post->id, "medium"); ?>
 			<figure>
 			<a href="<?php echo wp_get_attachment_url($post->id); ?>" title="<?php the_title(); ?>" rel="attachment"><img src="<?php echo $att_image[0];?>" width="<?php echo $att_image[1];?>" height="<?php echo $att_image[2];?>"  class="attachment-medium" alt="<?php $post->post_excerpt; ?>" /></a>
-			<figcaption><?php if ( !empty($post->post_excerpt) ) the_excerpt() ?></figcaption>
+			<figcaption><?php if ( !empty($post->post_excerpt) ) echo "$post->post_excerpt" ?></figcaption>
 			</figure>
 <?php else : ?>
 			<a href="<?php echo wp_get_attachment_url($post->ID) ?>" title="<?php echo esc_html( get_the_title($post->ID), 1 ) ?>" rel="attachment"><?php echo basename($post->guid) ?></a>
-			<div><?php if ( !empty($post->post_excerpt) ) the_excerpt() ?></div>
+			<div><?php if ( !empty($post->post_excerpt) ) echo "$post->post_excerpt" ?></div>
 <?php endif; ?>
 			
 			<footer class="post-meta">
