1c47e04dc4f77eecb3de16610565f5b1d28023f6
Tyler Longren <tlongren@gmail.com>
2011-06-10 10:27:24 -0500

    add role="contentinfo"

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <?php global $html5press_options; $html5press_settings = get_option( 'html5press_options', $html5press_options ); ?>
-    <footer id="footer" class="grid_12">
+    <footer id="footer" role="contentinfo">
         <!-- You're free to remove the credit link in the footer, but please, please leave it there. -->
         <p><?php _e( 'Copyright ','html5press' ); ?>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>" title="<?php _e( 'This Website','html5press' ); ?>"><?php bloginfo('name'); ?></a> - <?php _e( 'Design by ','html5press' ); ?><a href="http://jayj.dk" title="Design by Jayj.dk">Jayj.dk</a><?php _e( ' & WordPress\'d by ','html5press' ); ?><a href="http://www.longren.org/wordpress/html5press/">Tyler Longren</a></p>
 	<p>
