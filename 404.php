ff437731212af67b2c6133ce5721d95105b0a597
Tyler Longren <tlongren@gmail.com>
2011-06-15 15:24:57 -0500

    Add missing echo before home_url().

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 <div id="content" role="main">
 	<article class="hentry">
         <h1 class="entry-title"><?php _e( 'Error 404','html5press'); ?></h1>
-		<p><?php _e( 'The page you requested could not be found. Try searching or going back to the ','html5press'); ?><a href="<?php home_url(); ?>"><?php _e( 'home page','html5press'); ?></a>.</p>
+		<p><?php _e( 'The page you requested could not be found. Try searching or going back to the ','html5press'); ?><a href="<?php echo home_url(); ?>"><?php _e( 'home page','html5press'); ?></a>.</p>
 		<div class="clear"></div>
 	</article> <!-- end post 1 --> 
 </div> <!-- end main -->
