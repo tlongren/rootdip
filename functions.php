cda0a367c2bdc3dd9936068a2c73c14ec444c0bb
Tyler Longren <tlongren@gmail.com>
2011-09-09 15:59:59 -0500

    Version bump.

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ function html5press_layout_view() {
 if ( ! isset( $content_width ) ) $content_width = 580;
 
 // Set html5press version
-define( 'html5press_version', '2.0' );
+define( 'html5press_version', '2.1-rc1' );
 function html5press_getinfo( $show = '' ) {
         $output = '';
 
