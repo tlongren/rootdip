c4a5ea53f4d97206065f186147b0024ae3943070
Tyler Longren <tlongren@gmail.com>
2011-10-16 20:18:24 -0500

    No need for get_stylesheet_directory_uri.

--- functions.php
+++ functions.php
@@ -40,9 +40,9 @@ function html5press_theme_setup() {
 	add_theme_support( 'post-thumbnails' ); // post thumbnails
 	register_nav_menu( 'main-menu', __('Main Menu','html5press') ); // navigation menus
 	add_theme_support( 'automatic-feed-links' ); // automatic feeds
-	add_image_size('bxthumb', 200, 200, true); // featured post slider image size
+	add_image_size( 'bxthumb', 200, 200, true ); // featured post slider image size
 	add_custom_background( 'html5press_custom_background_callback' ); // enable custom backgrounds
-	add_editor_style(get_stylesheet_directory_uri() . '/css/editor-style.css');
+	add_editor_style( '/css/editor-style.css' );
 }
 
 // Register all the javascript
