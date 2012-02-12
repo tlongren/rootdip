10106d62fd8b30c1fcd28f57b0c4bd2477accd2c
Tyler Longren <tlongren@gmail.com>
2012-02-12 14:06:29 -0600

    / ‘search.php’

--- search.php
+++ search.php
@@ -13,7 +13,7 @@
             <?php if ( has_post_thumbnail() ) { ?><figure><a href="<?php the_permalink(); ?>"><img src="<?php echo "$image[0]"; ?>" alt="" class="thumbnail alignleft" /></a></figure><?php } ?>
             
             <?php the_content(__( 'Read more','html5press' )); ?>
-            
+            <?php /* Edit Link */ edit_post_link(); ?>
 			<footer class="post-meta">
 				<p>
                     <?php _e( 'In ','html5press'); ?><?php the_category(', '); ?>
@@ -21,7 +21,7 @@
                     <?php _e( 'on ','html5press'); ?> <time datetime="<?php echo get_the_time('Y-m-d'); ?>" pubdate><?php echo get_the_time( get_option( 'date_format' ) ); ?></time><?php if (get_the_modified_time() != get_the_time()) { ?>, updated <time datetime="<?php the_modified_time('Y-m-d\TH:i:sO'); ?>"><?php the_modified_date(); ?></time><?php } ?>
 					<?php wp_link_pages( array( 'before' => __( '<span class="alignright">Pages:', 'html5press' ), 'after' => '</span>' ) ); ?>
 				</p>
-				<?php /* Edit Link */ edit_post_link(); ?>
+				
             </footer> <!-- end post meta -->
 			<article class="comments">
 				<?php comments_template(); ?>
