1a5206d9fd1a0832c28d30d9190df1f1bed1d59f
Jesper Johansen <kontakt@jayj.dk>
2012-03-29 20:24:17 +0200

    Update footer.php to the new option functions and make use of printf to make it easier for translators to translate.
    @suggestion Move the JS out of this file into their own function and attach it the wp_footer action.

--- footer.php
+++ footer.php
@@ -1,38 +1,57 @@
-    <?php global $html5press_options; $html5press_settings = get_option( 'html5press_options', $html5press_options ); ?>
-    <footer id="footer" role="contentinfo">
-        <!-- You're free to remove the credit link in the footer, but please, please leave it there. -->
-        <p><?php _e( 'Copyright ','html5press' ); ?>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>" title="<?php _e( 'This Website','html5press' ); ?>"><?php bloginfo('name'); ?></a> - <?php _e( 'Design by ','html5press' ); ?><a href="http://jayj.dk" title="Design by Jayj.dk">Jayj.dk</a><?php _e( ' & WordPress\'d by ','html5press' ); ?><a href="http://www.longren.org/wordpress/html5press/">Tyler Longren</a></p>
-	<p>
-	<?php 
-	printf( __( 'Powered by %1$s and %2$s','html5press' ),
-		sprintf('<a href="http://wordpress.org/">%1$s</a>',
-			__( 'WordPress','html5press' )
-		),
-		sprintf('<a href="http://www.longren.org/wordpress/html5press/" title="%1$s">HTML5Press %2$s</a>',
-			__( 'HTML5Press WordPress Theme.','html5press' ),
-			html5press_getinfo('version')
-		)
-	);?>
-	<?php
-	if ($html5press_settings['show_query_stats'] == 1) { ?>
-		 - <?php echo get_num_queries(); _e( ' queries in ','html5press' ); timer_stop(1); _e( ' seconds','html5press' ); } ?>
-	</p>
-	</footer> <!-- end footer -->
-    
-    <div class="clear"></div>
+<?php
+	$options = html5press_get_options();
+?>
 
-</div> <!-- end wrapper -->
-	<?php if ($html5press_settings['back_to_top'] == 1) { ?>
+	<footer id="footer" role="contentinfo">
+		<!-- You're free to remove the credit links in the footer, but please, please leave it there. -->
+		<p>
+			<?php
+				/* translators: 1: year, 2: blog name, 3: link to designer 4: link to theme */
+				printf( __( 'Copyright &copy; %1$s %2$s - Design by %3$s and & WordPress\'d by %4$s','html5press' ),
+					date('Y'),
+					'<a href="' . home_url() . '" title="' . get_bloginfo('name') . '">' . get_bloginfo('name') . '</a>',
+					'<a href="http://jayj.dk" title="Design by Jayj.dk">Jayj.dk</a>',
+					'<a href="http://www.longren.org/wordpress/html5press/">Tyler Longren</a>'
+				);
+			?>
+		</p>
+
+		<p>
+			<?php 
+				printf( __( 'Powered by %1$s and %2$s','html5press' ),
+					sprintf('<a href="http://wordpress.org/">%1$s</a>',
+						__( 'WordPress','html5press' )
+					),
+					sprintf('<a href="http://www.longren.org/wordpress/html5press/" title="%1$s">HTML5Press %2$s</a>',
+						__( 'HTML5Press WordPress Theme.','html5press' ),
+						html5press_getinfo('version')
+					)
+				);
+			?>
+
+			<?php
+				if ( $options['show_query_stats'] == 1) {
+					printf( __( '- %1$s queries in %2$s seconds','html5press' ), get_num_queries(), timer_stop(1) );
+				}
+			?>
+		</p>
+	</footer> <!-- end #footer -->
+	
+	<div class="clear"></div>
+
+</div> <!-- end #wrapper -->
+
+	<?php if ($options['back_to_top'] == 1) { ?>
 	<script type="text/javascript">
 	jQuery(document).ready(function() {		
 		jQuery().UItoTop({ easingType: 'easeOutQuart',text: 'Back To Top',min: '300'});
 	});
 	</script>
 	<?php } ?>
-	<?php if ($html5press_settings['fuzzy_timestamps'] == 1) { ?>
+	<?php if ($options['fuzzy_timestamps'] == 1) { ?>
 	<script type="text/javascript">
 	jQuery(document).ready(function() {
-  		jQuery("time.timeago").timeago();
+		jQuery("time.timeago").timeago();
 	});
 	</script>
 	<?php } ?>
