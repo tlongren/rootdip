b2787d2ecd392783088d4b0989ae0696347d1828
Tyler Longren <tlongren@gmail.com>
2012-03-30 15:45:46 -0500

    Use wp_enqueue_scripts() to add styles instead of wp_print_styles(). See http://wpdevel.wordpress.com/2011/12/12/use-wp_enqueue_scripts-not-wp_print_styles-to-enqueue-scripts-and-styles-for-the-frontend/ Create two new option functions. One to get the default options and one to return an array with the options. This will elimate the need of global variables. Update attachments.php
    * Use the new option functions
    * Fix notice caused by lowercase $post->id
    * Code clean-up
    * Use of printf in the i18n strings
    Update footer.php to the new option functions and make use of printf to make it easier for translators to translate.
    
    @suggestion Move the JS out of this file into their own function and attach it the wp_footer action.
    Update header.php to the new options functions.
    
    Also specify 'theme_location' in the wp_nav_menu() call.

--- attachment.php
+++ attachment.php
@@ -1,36 +1,69 @@
 <?php get_header(); ?>
-<?php global $html5press_options; $html5press_settings = get_option( 'html5press_options', $html5press_options ); ?>
+
+<?php
+	$options = html5press_get_options();
+?>
+
 <div id="content" role="main">
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <article <?php post_class(); ?>>
-        
-            <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-			<div class="alignleft prev-post"><?php previous_image_link(false,__('Previous Attachment','html5press')); ?></div><div class="alignright next-post"><?php next_image_link(false,__('Next Attachment','html5press')); ?></div>
-			<div class="clear"></div>
-<?php if ( wp_attachment_is_image( $post->id ) ) : $att_image = wp_get_attachment_image_src( $post->id, "medium"); $large_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $html5press_settings['featured_image_size'] ); ?>
+
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+	<article <?php post_class(); ?>>
+	
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+		<div class="alignleft prev-post">
+			<?php previous_image_link( false, __( 'Previous Attachment', 'html5press' ) ); ?>
+		</div>
+
+		<div class="alignright next-post">
+			<?php next_image_link( false, __( 'Next Attachment','html5press' ) ); ?>
+		</div>
+		
+		<div class="clear"></div>
+
+		<?php
+			if ( wp_attachment_is_image() ) :
+				$att_image   = wp_get_attachment_image_src( $post->ID, 'medium' );
+				$large_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $options['featured_image_size'] );
+				$excerpt     = $post->post_excerpt;
+		?>
+
 			<figure>
-			<a href="<?php echo "$large_image[0]"; ?>" title="<?php the_title(); ?>" rel="lightbox"><img src="<?php echo $att_image[0];?>" width="<?php echo $att_image[1];?>" height="<?php echo $att_image[2];?>"  class="attachment-medium" alt="<?php $post->post_excerpt; ?>" /></a>
-			<figcaption><?php if ( !empty($post->post_excerpt) ) echo "$post->post_excerpt" ?></figcaption>
+				<a href="<?php echo esc_url( $large_image[0] ); ?>" title="<?php the_title(); ?>" rel="lightbox">
+					<img src="<?php echo esc_url( $att_image[0] ); ?>" width="<?php echo esc_attr( $att_image[1] ); ?>" height="<?php echo esc_attr( $att_image[2] ); ?>" class="attachment-medium" alt="<?php esc_attr( $excerpt ); ?>" />
+				</a>
+
+				<figcaption><?php if ( ! empty( $excerpt ) ) echo $excerpt; ?></figcaption>
 			</figure>
-			<p><a href="<?php echo get_permalink($post->post_parent); ?>">&laquo;<?php _e( 'Back to Post','html5press' ); ?></a></p>
-<?php else : ?>
-			<a href="<?php echo wp_get_attachment_url($post->ID) ?>" title="<?php echo esc_html( get_the_title($post->ID), 1 ) ?>" rel="attachment"><?php echo basename($post->guid) ?></a>
-			<div><?php if ( !empty($post->post_excerpt) ) echo "$post->post_excerpt" ?></div>
-<?php endif; ?>
-			<?php /* Edit Link */ edit_post_link(); ?>
-			<footer class="post-meta">
-                <p>
-                	<?php _e( 'Created ','html5press'); ?><time datetime="<?php the_time('Y-m-d\TH:i:sO'); ?>" pubdate><?php the_time( get_option( 'date_format' ) ); ?></time>
-				</p>
-				
-            </footer> <!-- end post meta -->
-			<article class="comments">
-				<?php comments_template(); ?>
-			</article>
-        </article> <!-- end post 1 -->
-		<?php endwhile; endif; ?>
-    
-    </div> <!-- end main -->
+
+			<p><a href="<?php echo get_permalink( $post->post_parent ); ?>">&laquo;<?php _e( 'Back to Post','html5press' ); ?></a></p>
+
+		<?php else : ?>
+
+			<a href="<?php echo wp_get_attachment_url($post->ID) ?>" title="<?php echo esc_html( get_the_title($post->ID), 1 ) ?>" rel="attachment"><?php echo basename( $post->guid ); ?></a>
+		
+			<div><?php if ( ! empty( $excerpt ) ) echo $excerpt; ?></div>
+
+		<?php endif; ?>
+
+		<?php /* Edit Link */ edit_post_link(); ?>
+
+		<footer class="post-meta">
+			<p>
+				<?php printf( __( 'Created on %s', 'html5press' ), '<time datetime="' . get_the_date('Y-m-d\TH:i:sO') . '" pubdate>' . get_the_date( get_option( 'date_format' ) ) . '</time>' ); ?>
+			</p>
+		</footer> <!-- .post-meta -->
+
+		<article class="comments">
+			<?php comments_template(); ?>
+		</article>
+
+	</article> <!-- end post 1 -->
+
+	<?php endwhile; endif; ?>
+	
+</div> <!-- #content -->
 
 <?php get_sidebar(); ?>
 
--- header.php
+++ header.php
@@ -1,77 +1,63 @@
-<?php global $html5press_options; $html5press_settings = get_option( 'html5press_options', $html5press_options ); if (!isset($html5press_settings['theme_color'])) { $html5press_settings['theme_color'] = "pink"; } ?>
+<?php
+	$options = html5press_get_options();
+?>
 <!doctype html>
 <!--[if lt IE 7]> <html class="no-js ie6" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>    <html class="no-js ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>    <html class="no-js ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
-
-    <meta charset="utf-8" />
-    
-    <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+	<meta charset="utf-8" />
+	
+	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    
-    <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+	
+	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
 	
 	<!-- Mobile viewport optimized: j.mp/bplateviewport -->
- 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" />
-	<link href="<?php echo get_stylesheet_directory_uri(); ?>/css/<?php echo esc_attr( $html5press_settings['theme_color'] ); ?>.css" rel="stylesheet" />
-    
+	<link href="<?php echo get_stylesheet_directory_uri(); ?>/css/<?php echo esc_attr( $options['theme_color'] ); ?>.css" rel="stylesheet" />
+	
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-<?php wp_head(); ?>
-<?php if (($html5press_settings['infinite_scroll'] == 1) && (!is_singular())) { ?>
-<script type="text/javascript">
-jQuery(document).ready(function($) {
-        var count = 2;
-        var total = <?php echo $wp_query->max_num_pages; ?>;
-        $(window).scroll(function(){
-                if  ($(window).scrollTop() == $(document).height() - $(window).height()){
-                   if (count > total){
-                        return false;
-                   }else{
-                        loadArticle(count);
-                   }
-                   count++;
-                }
-        }); 
+	
+	<?php wp_head(); ?>
 
-        function loadArticle(pageNumber){    
-                $('div#inifiniteLoader').show('fast');
-                $.ajax({
-                    url: "<?php bloginfo('wpurl') ?>/wp-admin/admin-ajax.php",
-                    type:'POST',
-                    data: "action=infinite_scroll&page_no="+ pageNumber + '&loop_file=loop', 
-                    success: function(html){
-                        $('div#inifiniteLoader').hide('1000');
-                        $("#content").append(html);    // This will be the div where our content will be loaded
-                    }
-                });
-            return false;
-        }
-
-    });
-</script>
-<?php } ?>
-<?php if ( !empty($html5press_settings['custom_css']) ) { ?><style type="text/css"><?php echo $html5press_settings['custom_css']; ?></style><?php } ?>
+	<?php if ( ! empty( $options['custom_css'] ) ) { ?>
+		<style><?php echo $options['custom_css']; ?></style>
+	<?php } ?>
 </head>
 
 <body <?php body_class(); ?>>
 
 <div id="wrapper">
 
-    <header id="header" class="clearfix" role="banner">
+	<header id="header" class="clearfix" role="banner">
 	
 		<hgroup>
-			<h1 id="site-title"><a href="<?php echo home_url(); ?>"><?php if (empty($html5press_settings['custom_logo_url'])) { bloginfo('name'); } else { ?><img src="<?php echo $html5press_settings['custom_logo_url']; ?>" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>" /><?php } ?></a></h1>
-			<?php if ($html5press_settings['show_tagline'] == 1) { ?><h2 id="site-description"><?php bloginfo('description'); ?></h2><?php } ?>
+			<h1 id="site-title">
+				<a href="<?php echo home_url(); ?>">
+					<?php
+						if ( empty( $options['custom_logo_url'] ) ) {
+							bloginfo( 'name' );
+						} else {
+					?>
+						<img src="<?php echo esc_url( $options['custom_logo_url'] ); ?>" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>" />
+					<?php } ?>
+				</a>
+			</h1>
+
+			<?php if ( $options['show_tagline'] ) { ?>
+				<h2 id="site-description"><?php bloginfo('description'); ?></h2>
+			<?php } ?>
 		</hgroup>
 		
-    </header> <!-- end header -->
+	</header> <!-- end header -->
 
 <div id="main" class="clearfix">
-    <!-- Navigation -->
+	
+	<!-- Navigation -->
 	<nav id="menu" class="clearfix" role="navigation">
 		<h2 class="assistive-text"><?php _e( 'Main menu', 'html5press' ); ?></h2> 
-		<?php wp_nav_menu( array( 'menu' => 'top-menu', 'container' => 'false' ) ); ?>
+		<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'menu' => 'top-menu', 'container' => 'false' ) ); ?>
 	</nav> <!-- #nav -->
