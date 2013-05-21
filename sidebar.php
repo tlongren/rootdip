31fa53e6d30120a1af16aff632aa35325ebef992
tlongren <tlongren@gmail.com>
2013-05-20 23:25:20 -0500

    Add more html5 tags to widget area, switch h2's out for h3's. Also change internal asside tags to section tag.

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div id="sidebar" role="complementary">
+<aside id="sidebar" role="complementary">
 <?php
 if ( is_active_sidebar( 'right-sidebar' ) ) : ?>
 <?php
@@ -10,28 +10,28 @@ else:
 <?php get_search_form(); ?>
 </aside>
 
-<aside class="widget">
-	<h2 class="widget-title"><?php _e( 'Archives','html5press' ); ?></h2>
+<section class="widget">
+	<h3 class="widget-title"><?php _e( 'Archives','html5press' ); ?></h3>
 	<ul>
 		<?php wp_get_archives( array('type' => 'monthly') ); ?>
 	</ul>
-</aside>
+</section>
 
-<aside class="widget">
-	<h2 class="widget-title"><?php _e( 'Categories','html5press' ); ?></h2>
+<section class="widget">
+	<h3 class="widget-title"><?php _e( 'Categories','html5press' ); ?></h3>
 	<ul>
 		<?php wp_list_categories( array('title_li' => false,'depth' => '-1') ); ?>
 	</ul>
-</aside>
+</section>
 
-<aside class="widget">
-	<h2 class="widget-title"><?php _e( 'Meta','html5press' ); ?></h2>
+<section class="widget">
+	<h3 class="widget-title"><?php _e( 'Meta','html5press' ); ?></h3>
 	<ul>
 		<?php wp_register(); ?>
 		<li><?php wp_loginout(); ?></li>
 		<?php wp_meta(); ?>
 	</ul>
-</aside>
+</section>
 <?php endif; ?>
-</div> <!-- end sidebar -->
+</aside> <!-- end sidebar -->
  </div> <!-- end content -->
