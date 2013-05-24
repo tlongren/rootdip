598a072655091f5feefa13c9ef33c7cc3478e2a2
tlongren <tlongren@gmail.com>
2013-05-23 22:55:54 -0500

    Remove check for infinite scroll option.

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 <?php $options = html5press_get_options(); ?>
 <main id="content" role="main">
 		<?php get_template_part( 'loop', 'index' ); ?>
-	<?php if ($options['infinite_scroll'] != 1) { ?>
 	<h2 class="assistive-text"><?php _e( 'Post navigation', 'html5press' ); ?></h2>
 	<nav class="navigation">
 		<div class="nav-previous alignleft"><?php previous_posts_link( __( '<span class="meta-nav">&larr;</span> Newer Posts','html5press' ) ); ?></div>
 		<div class="nav-next alignright"><?php next_posts_link( __( 'Older Posts <span class="meta-nav">&rarr;</span>','html5press' ) ); ?></div>
 	</nav>
-	<?php } ?>
     </main> <!-- end main -->
 
 <?php get_sidebar(); ?>
