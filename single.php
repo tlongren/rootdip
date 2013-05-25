2612769aa1288568f32c50fc6a91688de95cca0a
tlongren <tlongren@gmail.com>
2013-05-24 21:44:25 -0500

    Add author info, so fill out your author profiles people.

--- single.php
+++ single.php
@@ -25,7 +25,20 @@
 			</p>
 			
 			</footer> <!-- .post meta -->
-			
+			<div id="author-info">
+			<div id="author-avatar">
+				<?php echo get_avatar( get_the_author_meta( 'user_email' ), '75' ); ?>
+			</div><!-- #author-avatar -->
+			<div id="author-description">
+				<h2><?php printf( __( 'About %s', 'html5press' ), get_the_author() ); ?></h2>
+				<?php the_author_meta( 'description' ); ?>
+				<div id="author-link">
+					<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
+						<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'html5press' ), get_the_author() ); ?>
+					</a>
+				</div><!-- #author-link	-->
+			</div><!-- #author-description -->
+			</div><!-- #author-info -->
 			<article class="comments">
 				<?php comments_template(); ?>
 			</article>
